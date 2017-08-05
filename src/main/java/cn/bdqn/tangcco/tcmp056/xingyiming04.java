ef317a371491b82b59373bc0c7f75ab5dab6eb1d
‘xingyiming’ <260892184@qq.com>
2017-08-05 21:31:22 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/xingyiming04.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/5.
+ */
+public class xingyiming04 {
+    @Test
+    public void xingyiming(){
+        System.out.println("holle word");
+    }
+}
